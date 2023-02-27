ee89b3511cb72afbad18111d3b4ac92ef6cbf488
lomeat <lom3at@gmail.com>
2023-02-28 00:19:29 +0400

    add keyboard handlers

---
+++ src/keyboard-controller.ts
@@ -0,0 +1,50 @@
+import { Ticker } from "pixi.js";
+
+type KeyboardState = {
+  key: string;
+  isDown: boolean;
+};
+
+type KeyboardProps = {
+  key: string;
+  onPress?: () => void;
+  onRelease?: () => void;
+  action: (dt) => void;
+};
+
+export function KeyboardController(props: KeyboardProps) {
+  const state: KeyboardState = {
+    key: props.key,
+    isDown: false,
+  };
+
+  function handleDown(e) {
+    if (e.key === state.key) {
+      if (!state.isDown && props.action) {
+        Ticker.shared.add(props.action);
+      }
+      state.isDown = true;
+      e.preventDefault();
+    }
+  }
+
+  function handleUp(e) {
+    if (e.key === state.key) {
+      if (state.isDown && props.action) {
+        Ticker.shared.remove(props.action);
+      }
+      state.isDown = false;
+      e.preventDefault();
+    }
+  }
+
+  function unsubscribe() {
+    window.removeEventListener("keydown", handleDown);
+    window.removeEventListener("keyup", handleUp);
+  }
+
+  window.addEventListener("keydown", handleDown, false);
+  window.addEventListener("keyup", handleUp, false);
+
+  return { ...state, unsubscribe };
+}
