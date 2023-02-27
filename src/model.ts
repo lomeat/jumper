3790f7519db157cd16adb8b60decd3bd6a92fa7c
lomeat <lom3at@gmail.com>
2023-02-28 02:17:59 +0400

    refactor player movement logic, shorter code

--- src/model.ts
+++
@@ -1,5 +0,0 @@
-export type Player = {
-  color: number;
-  speed: number;
-  isAlive: boolean;
-};
--- src/player.ts
+++ src/player.ts
@@ -1,22 +1,20 @@
-import { Application, Sprite, Texture, Ticker } from "pixi.js";
+import { Application, Sprite, StateSystem, Texture, Ticker } from "pixi.js";
 
-import { generateHexColor } from "./utils";
 import { KeyboardController } from "./keyboard-controller";
 import * as Model from "./model";
 import { gameState } from "./state";
+import { EPlayerDirections, EAxis } from "./constants";
 
 type Props = {
   sizes?: [number, number];
 };
-
-type Direction = "left" | "right" | "up" | "down";
 
 export function Player(props?: Props) {
   // [init]
 
   const sprite = new Sprite(Texture.WHITE);
 
-  const state: Model.Player = { ...gameState.player };
+  const state: Model.Player.State = gameState.player;
 
   sprite.width = props?.sizes?.[0] ?? 100;
   sprite.height = props?.sizes?.[1] ?? 200;
@@ -25,49 +23,29 @@ export function Player(props?: Props) {
   sprite.tint = state.color;
   sprite.interactive = true;
 
-  const movement = {
-    left: KeyboardController({
-      key: "a",
-      action: (dt) => move(dt, "left"),
+  const movement = Object.entries(gameState.controls.movement).reduce(
+    (acc, [key, value]) => ({
+      ...acc,
+      [key]: KeyboardController({
+        key: value,
+        action: (dt) => move(dt, key as Model.Player.Direction),
+      }),
     }),
-    right: KeyboardController({
-      key: "d",
-      action: (dt) => move(dt, "right"),
-    }),
-    up: KeyboardController({
-      key: "w",
-      action: (dt) => move(dt, "up"),
-    }),
-    down: KeyboardController({
-      key: "s",
-      action: (dt) => move(dt, "down"),
-    }),
-  };
+    // In my opinion, there is unsafe 'as'
+    {} as Model.Player.Movement
+  );
 
   // [methods]
 
-  function move(dt, direction: Direction) {
-    switch (direction) {
-      case "left":
-        sprite.x += dt * state.speed * -1;
-        break;
-      case "right":
-        sprite.x += dt * state.speed * 1;
-        break;
-      case "up":
-        sprite.y += dt * state.speed * -1;
-        break;
-      case "down":
-        sprite.y += dt * state.speed * 1;
-        break;
-      default:
-        break;
-    }
+  function move(dt, direction: Model.Player.Direction) {
+    // Update sprite 'x' or 'y' position
+    // Direction's enum returns '1' or '-1'
+    sprite[EAxis[direction]] += dt * state.speed * EPlayerDirections[direction];
   }
 
   function checkAlive() {
     if (!state.isAlive) {
-      removeControls();
+      removeMovementListeners();
     }
   }
 
@@ -75,7 +53,7 @@ export function Player(props?: Props) {
     state.speed = newSpeed;
   }
 
-  function removeControls() {
+  function removeMovementListeners() {
     movement.left.unsubscribe();
     movement.right.unsubscribe();
     movement.up.unsubscribe();
@@ -86,7 +64,7 @@ export function Player(props?: Props) {
 
   Ticker.shared.add(checkAlive);
 
-  const newProps = { move, setSpeed, removeControls };
+  const newProps = { move, setSpeed, removeMovementListeners };
 
   return Object.assign(sprite, newProps);
 }
